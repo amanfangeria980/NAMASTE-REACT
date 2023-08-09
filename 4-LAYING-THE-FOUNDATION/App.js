6c6d0db00c4c2a928996430944cf22ba7320dbcc
amanfangeria980 <amanfangeria980@gmail.com>
2023-08-10 02:03:59 +0530

    started writing jsx

--- 4-LAYING-THE-FOUNDATION/App.js
+++ 4-LAYING-THE-FOUNDATION/App.js
@@ -1,45 +1,50 @@
-/* 
-WHAT PARCEL IS DOING?
-Created a server
-Hot module replacement - HMR
-File Watcher algorithm - C++
-BUNDLING
-{npx build parcel index.html for prod build, remove entrypoint from package.json as we have to pass it in the command line itself in parcel}
-MINIFY
-Cleaning our code
-Dev and Production Build
-Super fast build algorithm
-Image Optimization
-Caching while development
-Compression
-Compatible with older versions of browser
-HTTPS on dev
-Manages port numbers
-Consistent Hashing Algorithm to do caching
-Zero-Config Bundler
-Uses Transitive Dependencies
-
-
-*/
-
-
 import React from "react";
+import { createElement } from "react";
+// import { createElement as ce } from "react";
+
 import ReactDOM from "react-dom/client";
+// React.createElement => Object => HTMl DOM
 
-const heading1=React.createElement("h1",{
-    id:"heading-tag",
-},"Hello React!");
+/* <div class="header">
+    <h1>Namaste React</h1>
+    <ul>
+    <li>About Us</li>
+    <li>Contact Us</li>
+    <li>Call Us</li>
+    </ul>
+</div> */
 
-const heading2=React.createElement("h2",{
-    id:"title",
-},"Heading 2")
+//We will not write this from now on, instead we will use JSX
 
-const container=React.createElement("div",{
-    id: "container",
-    style: {
-        backgroundColor: "red"
-    },
-},[heading1,heading2]);
+const container = createElement(
+  "div",
+  {
+    className: "header",
+  },
+  [
+    createElement("h1", {}, "Namaste React"),
+    createElement("ul", {}, [
+      createElement("li", {}, "About Us"),
+      createElement("li", {}, "Contact Us"),
+      createElement("li", {}, "Call Us"),
+    ]),
+  ]
+);
 
-const root= ReactDOM.createRoot(document.getElementById("root"));
+// Writing html dom like this is a pain,so that's why we are gonna use JSX, for example
+
+// This is executed using babel : creates ABS (abstract syntax tree) it's different from ESTree 
+//This is a JSX expression
+const heading2 = (
+  <h1 id="title" key="101" tabIndex="1" className="heading1">
+    Aman Fangeria
+  </h1>
+);
+
+// JSX => React.createElement => HTML(DOM) 
+// There are lots of benefits of JSX => readability, less code, etc...
+
+// JSX is not html inside js instead, it is html like something or fancy way of writing html inside js
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
