9597068a905459f5b9a0f7be5eb37d60c95770d4
amanfangeria980 <amanfangeria980@gmail.com>
2023-08-30 18:24:53 +0530

    Intro to useState hook, why we need it

--- 6-LETS-GET-HOOKED/src/components/Body.js
+++ 6-LETS-GET-HOOKED/src/components/Body.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
+
+
+
 const Body = () => {
+    let searchTxt="KFC";
+    // React follows one-way data binding.
+
+    // The value doesn't change as react doesn't use local variable when we want to maintain a variable, we have to use some react variable or kind of a state variable.
+
+    // Every Component in react maintains a state, so we can put some variables on to that state and everytime we have to create some local variable, we can use state in it.
+    
+    // We will do this using useState hook
+    const [searchText]=useState();  
   return (
     <>
     <div className="search-container">
-        
+        <input type="text" className="search-input" placeholder="Search" value={searchText} onChange={(e)=>searchText=e.target.value}/>
+        {/* will not work or change the text, reason up there */}
+        <button className="search-btn">Search</button>
     </div>
       <div className="restaurant-list">
         {restaurantList.map((restaurant) => {
