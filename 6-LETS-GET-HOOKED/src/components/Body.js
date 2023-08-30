673a1b562d11e96f6f590330845c56118112ef20
amanfangeria980 <amanfangeria980@gmail.com>
2023-08-30 18:50:48 +0530

    we implemented useState hook on the search input text

--- 6-LETS-GET-HOOKED/src/components/Body.js
+++ 6-LETS-GET-HOOKED/src/components/Body.js
@@ -2,23 +2,25 @@ import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 
+// What is state and why
+// What is hook? and why
+// What is useState? and why  
 
 
 const Body = () => {
-    let searchTxt="KFC";
-    // React follows one-way data binding.
-
-    // The value doesn't change as react doesn't use local variable when we want to maintain a variable, we have to use some react variable or kind of a state variable.
-
-    // Every Component in react maintains a state, so we can put some variables on to that state and everytime we have to create some local variable, we can use state in it.
-    
+    // const searchText="KFC";
     // We will do this using useState hook
-    const [searchText]=useState();  
+    // searchText is a local state variable
+    // KFC is the default value being passed to that variable 
+    const [searchInput,setSearchInput]=useState("KFC"); //To create state variables  
   return (
     <>
     <div className="search-container">
-        <input type="text" className="search-input" placeholder="Search" value={searchText} onChange={(e)=>searchText=e.target.value}/>
+        {/* <input type="text" className="search-input" placeholder="Search" value={searchInput} onChange={(e)=>searchInput=e.target.value}/> */}
         {/* will not work or change the text, reason up there */}
+
+        {/* Instead, we will do this */}
+        <input type="text" className="search-input" placeholder="Search" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
         <button className="search-btn">Search</button>
     </div>
       <div className="restaurant-list">
