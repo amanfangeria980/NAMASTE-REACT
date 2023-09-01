fcb7553a59e18445f1b9977807bf67b28585652c
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-02 01:33:37 +0530

    we made a state variable using usestate hook and added toggle functionality to it

--- 6-LETS-GET-HOOKED/src/components/Body.js
+++ 6-LETS-GET-HOOKED/src/components/Body.js
@@ -2,27 +2,28 @@ import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 
-// What is state and why
-// What is hook? and why
-// What is useState? and why  
-
 
 const Body = () => {
-    // const searchText="KFC";
-    // We will do this using useState hook
-    // searchText is a local state variable
-    // KFC is the default value being passed to that variable 
-    const [searchInput,setSearchInput]=useState("KFC"); //To create state variables  
+    // useState hook returns an array, first is variable and second is function to update the variable
+    // Here it is using two way binding
+    const [searchInput,setSearchInput]=useState("KFC");
+    const [searchClicked,setSearchClicked]=useState("false");
   return (
     <>
-    <div className="search-container">
-        {/* <input type="text" className="search-input" placeholder="Search" value={searchInput} onChange={(e)=>searchInput=e.target.value}/> */}
-        {/* will not work or change the text, reason up there */}
 
-        {/* Instead, we will do this */}
+
+    <div className="search-container">
         <input type="text" className="search-input" placeholder="Search" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
-        <button className="search-btn">Search</button>
+        <h1>{searchClicked}</h1>
+        <button className="search-btn" onClick={()=>{
+          if(searchClicked==="true")setSearchClicked("false");
+          else setSearchClicked("true");
+        }}>Search</button>
+        {/* Here on click, our heading comonent is getting re rendered */}
+        {/* we don't need to add the event as we are changing the value ourselves, when we need any human interaction information or details, then only we use event or e */}
     </div>
+
+
       <div className="restaurant-list">
         {restaurantList.map((restaurant) => {
           return (
