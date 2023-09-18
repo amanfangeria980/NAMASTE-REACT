c7cfb83ad85d35b7783e296e82f8fabf060b179a
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-18 20:15:27 +0530

    minor changes added

--- 6-LETS-GET-HOOKED/src/App.js
+++ 6-LETS-GET-HOOKED/src/App.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header"
 import Body from "./components/Body";
 import Footer from "./components/Footer"
-import { IMG_CDN_URL } from "./constants";
 
 const AppLayout = () => (
   <>
