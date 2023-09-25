eaf98127b2ee405ef2b48a0b6b3a7c8a67e9630f
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-26 02:46:21 +0530

    fixed search feature by adding a new variable allrestaurants

--- 8-EXPLORING-THE-WORLD/src/components/Body.js
+++ 8-EXPLORING-THE-WORLD/src/components/Body.js
@@ -2,8 +2,8 @@ import { useState,useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from './Shimmer'
 
-function filterData(searchInput,restaurants){
-  const data=restaurantList.filter((restaurant)=>restaurant.info.name.includes(searchInput));
+function filterData(searchInput,allRestaurants){
+  const data=allRestaurants.filter((restaurant)=>restaurant.info.name.includes(searchInput));
   return data;
 }
 
@@ -11,36 +11,38 @@ function filterData(searchInput,restaurants){
 
 const Body = () => {
     const [searchInput,setSearchInput]=useState("");
-    const [restaurants, setRestaurants]=useState([]);
+    const [allRestaurants,setAllRestaurants]=useState([]);
+    const [filteredrestaurants, setFilteredRestaurants]=useState([]);
 
     async function getRestaurants(){
       const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
       const json=await data.json();
       console.log(json);
       // optional chaining to prevent error
-      setRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setAllRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilteredRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
-    
+
     useEffect(()=>{
       getRestaurants();
     },[])
 
 
-  return (restaurants.length===0) ? <Shimmer/> : (
+  return (filteredrestaurants.length===0) ? <Shimmer/> : (
     <>
     <div className="search-container">
         <input type="text" className="search-input" placeholder="Search" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
 
         <button className="search-btn" onClick={()=>{
-          const data=filterData(searchInput,restaurants);
-          setRestaurants(data);
+          const data=filterData(searchInput,allRestaurants);
+          setFilteredRestaurants(data);
         }}>Search</button>
 
     </div>
 
 
       <div className="restaurant-list">
-        {restaurants.map((restaurant) => {
+        {filteredrestaurants.map((restaurant) => {
           return (
             <RestaurantCard
               {...restaurant.info}
