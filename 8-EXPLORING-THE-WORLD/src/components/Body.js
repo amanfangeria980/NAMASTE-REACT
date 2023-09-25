a6477efe09c045f573e62fe67e7c25c476fb2819
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-26 02:38:13 +0530

    did some code restructuring

--- 8-EXPLORING-THE-WORLD/src/components/Body.js
+++ 8-EXPLORING-THE-WORLD/src/components/Body.js
@@ -13,25 +13,18 @@ const Body = () => {
     const [searchInput,setSearchInput]=useState("");
     const [restaurants, setRestaurants]=useState([]);
 
-    useEffect(()=>{
-      getRestaurants();
-    },[])
-
     async function getRestaurants(){
       const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
       const json=await data.json();
       console.log(json);
       // optional chaining to prevent error
       setRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-
-      // Implement Shimmer UI 
     }
     
-    // Conditional Rendering
-    // E.g.,
-    // if restaurant is empty -> shimmer UI 
-    // if restaurant has data -> actual data UI 
-    // We have done it below using ternary operator 
+    useEffect(()=>{
+      getRestaurants();
+    },[])
+
 
   return (restaurants.length===0) ? <Shimmer/> : (
     <>
