442e680163ab97666f27e4b28a7ac6e3b0e2f682
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-26 17:17:13 +0530

    useEffect learning--

--- 9-FINDING-THE-PATH/src/components/Body.js
+++ 9-FINDING-THE-PATH/src/components/Body.js
@@ -17,8 +17,7 @@ const Body = () => {
     async function getRestaurants(){
       const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
       const json=await data.json();
-      console.log(json);
-      // optional chaining to prevent error
+      // console.log(json);
       setAllRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
       setFilteredRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
@@ -57,5 +56,9 @@ const Body = () => {
 };
 
 export default Body;
+
+
+
+// if we don't pass any dependency array in useEffect, it will execute after every re render.
 
 
