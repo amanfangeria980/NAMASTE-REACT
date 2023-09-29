35b79f4dc7aa779ae2857f921fd792865c27c9da
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-29 19:59:10 +0530

    did some basic routing for home and about page

--- 9-FINDING-THE-PATH/src/App.js
+++ 9-FINDING-THE-PATH/src/App.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header"
 import Body from "./components/Body";
 import Footer from "./components/Footer"
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./components/About";
+
 
 const AppLayout = () => (
   <>
@@ -12,5 +15,17 @@ const AppLayout = () => (
   </>
 );
 
+// Always write after the AppLayout to prevent any errors
+const appRouter=createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>,
+  },
+  {
+    path:"/about",
+    element: <About/>
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
