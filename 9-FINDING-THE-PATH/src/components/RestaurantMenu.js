1cfdb74b498c1de53f11a27aec3603ad5719b536
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-30 04:33:06 +0530

    made our menu dynamic as per id

--- 9-FINDING-THE-PATH/src/components/RestaurantMenu.js
+++ 9-FINDING-THE-PATH/src/components/RestaurantMenu.js
@@ -9,7 +9,7 @@ const RestaurantMenu = () => {
 
   async function getRestaurantInfo() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=344841"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId="+resId
     );
     const json = await data.json();
     // console.log(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards);
