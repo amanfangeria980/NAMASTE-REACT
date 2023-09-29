d33a31e6ac154844660b25cf3aa707b44f919759
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-30 02:35:53 +0530

    fetching some data and rendering onto our restaurant menu

--- 9-FINDING-THE-PATH/src/components/RestaurantMenu.js
+++ 9-FINDING-THE-PATH/src/components/RestaurantMenu.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 
 const RestaurantMenu = () => {
   const {resId}=useParams();
+  
+
+  async function getRestaurantInfo(){
+    const data=await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=344841&catalog_qa=undefined&submitAction=ENTER");
+    const json=await data.json();
+    console.log(json);
+  }
+
+  useEffect(()=>{
+    getRestaurantInfo();
+  },[])
+
   return (
     <div>
         <h1>Restaurant id : {resId} </h1>
