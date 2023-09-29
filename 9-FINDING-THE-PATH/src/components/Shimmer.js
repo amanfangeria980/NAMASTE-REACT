97727cdaa8110725851b8bc09135e627ef20fde5
amanfangeria980 <amanfangeria980@gmail.com>
2023-09-29 17:50:17 +0530

    completed shimmer work

--- 9-FINDING-THE-PATH/src/components/Shimmer.js
+++ 9-FINDING-THE-PATH/src/components/Shimmer.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Shimmer = () => {
   return (
-    <h1>Shimmer UI Loading...</h1>
+    // <h1>Shimmer UI Loading...</h1>
+    <div className="restaurant-list">
+      {Array(18).fill("").map((e,index)=>{
+        return(
+          <div className='shimmer-card' id={index.toString()}></div>
+        )
+      })}
+    </div>
   )
 }
+
 
 export default Shimmer
