bc7c06fdd61ca7ac876dce7fe2adad1971258aba
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 02:01:06 +0530

    ---

--- 10_LETS-GET-CLASSY/src/components/ProfileClass.js
+++ 10_LETS-GET-CLASSY/src/components/ProfileClass.js
@@ -3,21 +3,17 @@ import React from 'react';
 class Profile extends React.Component{
     constructor(props){
         super(props);
-        // Create State Here! 
         this.state={
             count: 25,
         };
     }
 
     render(){
-        // can also destructure it and use like this:
-        // const {count}=this.state and directly use {count}
         return(
             <div>
                 <h1>Profile Class Component</h1>
                 <h2>Name: {this.props.name}</h2>
                 <h3>Count: {this.state.count}</h3>
-                {/* we do not mutate or change state directly */}
                 <button onClick={()=>{
                     this.setState({
                         count: 1,
