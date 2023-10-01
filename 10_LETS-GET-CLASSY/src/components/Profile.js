f0e054d034b8f926740bbe72eff072567d998c3b
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 04:10:17 +0530

    ---

--- 10_LETS-GET-CLASSY/src/components/Profile.js
+++ 10_LETS-GET-CLASSY/src/components/Profile.js
@@ -1,23 +1,9 @@
 import { useEffect, useState } from "react";
 const Profile = (props) => {
-  const [count] = useState(22);
-  useEffect(() => {
-    const timer=setInterval(() => {
-        console.log("HELLO AMAN");
-    }, 1000);
-    console.log("UseEffect");
-    return () => {
-        // This is kind of the unmounting phase
-        clearInterval(timer);
-      console.log("UseEffect Return");
-    };
-  }, []);
-  console.log("render");
   return (
     <div>
       <h1>Profile</h1>
       <h2>name: {props.name}</h2>
-      <h3>Count: {count}</h3>
     </div>
   );
 };
