82648202eb9c644ee6a444878188437685e7f7d3
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 01:30:18 +0530

    very important creating a nested route profile section under about

--- 10_LETS-GET-CLASSY/src/App.js
+++ 10_LETS-GET-CLASSY/src/App.js
@@ -9,6 +9,7 @@ import Error from "./components/Error";
 import Contact from "./components/Contact";
 import Cart from "./components/Cart";
 import RestaurantMenu from "./components/RestaurantMenu"
+import Profile from "./components/Profile";
 const AppLayout = () => (
   <>
     <Header />
@@ -28,8 +29,14 @@ const appRouter = createBrowserRouter([
         element: <Body/>,
       },
       {
-        path: "/about",
+        path: "/about", //parentPath/{path} => localhost:1234/about
         element: <About />,
+        children:[
+          {
+            path: "profile", //localhost:1234/about/profile or parentPath/{path}
+            element: <Profile/>
+          },
+        ]
       },
       {
         path: "/contact",
--- 10_LETS-GET-CLASSY/src/components/About.js
+++ 10_LETS-GET-CLASSY/src/components/About.js
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const About=()=>{
     return(
         <div>
             <h1>About Us Page</h1>
+            <Outlet/>
         </div>
     )
 }
