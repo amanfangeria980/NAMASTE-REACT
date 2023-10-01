43ec7c9e6595b7013776b995ae00bf7146bcc28f
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 04:09:10 +0530

    wrapped up complete useEffect

--- 10_LETS-GET-CLASSY/src/components/Profile.js
+++ 10_LETS-GET-CLASSY/src/components/Profile.js
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
-const Profile=(props)=>{
-    const [count]=useState(22);
-    useEffect(()=>{
-        console.log("UseEffect");
-        return()=>{
-            console.log("UseEffect Return");
-        }
-    },[]) 
-    console.log("render");
-
-    // So here what will happen is, first the initial page gets rendered, hence render will be printed, then useEffect will be printed, and when we leave that page, UseEffect return will be printed.
-    return(
-        <div>
-            <h1>Profile</h1>
-            <h2>name: {props.name}</h2>
-            <h3>Count: {count}</h3>
-
-        </div>
-    )
-}
+const Profile = (props) => {
+  const [count] = useState(22);
+  useEffect(() => {
+    const timer=setInterval(() => {
+        console.log("HELLO AMAN");
+    }, 1000);
+    console.log("UseEffect");
+    return () => {
+        // This is kind of the unmounting phase
+        clearInterval(timer);
+      console.log("UseEffect Return");
+    };
+  }, []);
+  console.log("render");
+  return (
+    <div>
+      <h1>Profile</h1>
+      <h2>name: {props.name}</h2>
+      <h3>Count: {count}</h3>
+    </div>
+  );
+};
 export default Profile;
