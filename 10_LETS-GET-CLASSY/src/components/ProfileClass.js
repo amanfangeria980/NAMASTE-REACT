13453df5450a634d02b88bee54abb5328379b644
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 01:37:26 +0530

    create a profileClass named class based component and use it in app.js

---
+++ 10_LETS-GET-CLASSY/src/components/ProfileClass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Profile extends React.Component{
+    // you cannot create a class-based component without render function
+    render(){
+        return(
+            <h1>Profile Class Component</h1>
+        )
+    }
+
+}
+
+export default Profile;
