de3f6530987f67556ad9577da682d9c848029219
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 01:49:29 +0530

    creating state variables using  useState in functional component

--- 10_LETS-GET-CLASSY/src/components/Profile.js
+++ 10_LETS-GET-CLASSY/src/components/Profile.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 const Profile=(props)=>{
+    const [count]=useState(22);
     return(
         <div>
             <h1>Profile</h1>
             <h2>name: {props.name}</h2>
+            <h3>Count: {count}</h3>
+
         </div>
     )
 }
