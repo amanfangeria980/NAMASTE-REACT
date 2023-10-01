758589e2d50a9651eaf12be31c68ade2b2a4f7ec
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-02 04:07:17 +0530

    what is useEffect return function

--- 10_LETS-GET-CLASSY/src/components/Profile.js
+++ 10_LETS-GET-CLASSY/src/components/Profile.js
@@ -1,6 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const Profile=(props)=>{
-    const [count]=useState(22); 
+    const [count]=useState(22);
+    useEffect(()=>{
+        console.log("UseEffect");
+        return()=>{
+            console.log("UseEffect Return");
+        }
+    },[]) 
+    console.log("render");
+
+    // So here what will happen is, first the initial page gets rendered, hence render will be printed, then useEffect will be printed, and when we leave that page, UseEffect return will be printed.
     return(
         <div>
             <h1>Profile</h1>
