f85b014f53420a27de3662347d87e9ded5ab90bb
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-05 20:12:45 +0530

    styled header

--- 12-JO-DIKHTA-HAI-WO-BIKTA-HAI/src/components/Header.js
+++ 12-JO-DIKHTA-HAI-WO-BIKTA-HAI/src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import useOnline from "../utils/useOnline"
 const Title = () => (
   <Link href="/">
-    <img className="logo" src={Logo} alt="logo" />
+    <img className="h-28 p-2" src={Logo} alt="logo" />
   </Link>
 );
 
@@ -15,19 +15,19 @@ const Header = () => {
   const [isLoggedIn,setIsLoggedIn]=useState(false);
   const isOnline=useOnline();
   return (
-    <div className="header">
+    <div className="flex justify-between bg-pink-50 shadow-lg">
       <Title />
-      <h1>{title}</h1>
+      <h1 className="py-10">{title}</h1>
       <div className="nav-items">
-        <ul>
-          <Link to="/"><li>Home</li></Link>
-          <Link to="/about"><li>About</li></Link>
-          <Link to="/contact"><li>Contact</li></Link>
-          <Link to="/cart"><li>Cart</li></Link>
-          <Link to="/instamart"><li>Instamart</li></Link>
+        <ul className="flex py-10" >
+          <Link to="/"><li className="px-2">Home</li></Link>
+          <Link to="/about"><li className="px-2">About</li></Link>
+          <Link to="/contact"><li className="px-2">Contact</li></Link>
+          <Link to="/cart"><li className="px-2">Cart</li></Link>
+          <Link to="/instamart"><li className="px-2">Instamart</li></Link>
         </ul>
       </div>
-      <h1>{isOnline ? "🟢" : "🔴" }</h1>
+      <h1 className="py-10">{isOnline ? "🟢" : "🔴" }</h1>
       {isLoggedIn ? <button onClick={()=>setIsLoggedIn(false)}>Logout</button> : <button onClick={()=>setIsLoggedIn(true)}>Login</button>}
     </div>
   );
