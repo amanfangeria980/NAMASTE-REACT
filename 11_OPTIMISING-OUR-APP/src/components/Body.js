693466d34baf9a907beac862faaf722754bf9d16
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-05 10:46:47 +0530

    --

--- 11_OPTIMISING-OUR-APP/src/components/Body.js
+++ 11_OPTIMISING-OUR-APP/src/components/Body.js
@@ -1,4 +1,4 @@
-import { useEffect, useState} from "react";
+import {useState} from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
