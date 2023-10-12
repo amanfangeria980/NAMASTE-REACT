6b5f577cd72f832713691166b9635b6161b35827
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-12 17:13:45 +0530

    styled shimmer

--- 13-DATA-IS-THE-NEW-OIL/src/components/Shimmer.js
+++ 13-DATA-IS-THE-NEW-OIL/src/components/Shimmer.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Shimmer = () => {
   return (
     // <h1>Shimmer UI Loading...</h1>
-    <div className="flex flex-wrap justify-between">
+    <div className="restaurant-list">
       {Array(18).fill("").map((e,index)=>{
         return(
-          <div className='h-48 w-48 bg-purple-200 m-5' key={index.toString()}></div>
+          <div className='shimmer-card' key={index.toString()}></div>
         )
       })}
     </div>
