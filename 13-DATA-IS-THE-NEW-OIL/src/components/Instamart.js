9acd3bb10d39dc239acbdd08ec44c32e728ea6e0
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-15 04:01:36 +0530

    hide feature also working

--- 13-DATA-IS-THE-NEW-OIL/src/components/Instamart.js
+++ 13-DATA-IS-THE-NEW-OIL/src/components/Instamart.js
@@ -38,7 +38,7 @@ const Instamart = () => {
       <Section
         isVisible={visibleSection === "about"}
         setIsVisible={() => {
-          setVisibleSection("about");
+          setVisibleSection(visibleSection === "about" ? "" : "about");
         }}
         title="About Instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
@@ -47,7 +47,7 @@ const Instamart = () => {
       <Section
         isVisible={visibleSection === "teams"}
         setIsVisible={() => {
-          setVisibleSection("teams");
+          setVisibleSection(visibleSection === "teams" ? "" : "teams");
         }}
         title="Team at Instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
@@ -56,7 +56,7 @@ const Instamart = () => {
       <Section
         isVisible={visibleSection === "careers"}
         setIsVisible={() => {
-          setVisibleSection("careers");
+          setVisibleSection(visibleSection === "careers" ? "" : "careers");
         }}
         title="Careers at Instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
