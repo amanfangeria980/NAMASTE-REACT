5ee3b114eea37f14c97ee9a9ecadd95f1966e38c
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-15 03:44:53 +0530

    build the show one and hide others accordion feature

--- 13-DATA-IS-THE-NEW-OIL/src/components/Instamart.js
+++ 13-DATA-IS-THE-NEW-OIL/src/components/Instamart.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
-const Section = ({ title, description }) => {
+const Section = ({ title, description,isVisible,setIsVisible }) => {
   // let's create a hide and unhide feature for our sections
-  const [isVisible, setIsVisible] = useState(false);
   return (
     <div className="border border-black p-2 m-2">
       <h3 className="font-bold text-xl">{title}</h3>
@@ -32,20 +31,37 @@ const Section = ({ title, description }) => {
 };
 
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({ 
+    showAbout:false,
+    showTeams: false,
+    showCareers: false,
+  });
   return (
     <div>
       <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
-      <Section
+      <Section isVisible={sectionConfig.showAbout} setIsVisible={()=>setSectionConfig({
+        showAbout: true,
+        showCareers: false,
+        showTeams: false,
+      })}
         title="About Instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
       />
 
-      <Section
+      <Section isVisible={sectionConfig.showTeams} setIsVisible={()=>setSectionConfig({
+        showAbout: false,
+        showCareers: false,
+        showTeams: true,
+      })}
         title="Team at Instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
       />
 
-      <Section
+      <Section isVisible={sectionConfig.showCareers} setIsVisible={()=>setSectionConfig({
+        showAbout: false,
+        showCareers: true,
+        showTeams: false,
+      })}
         title="Careers at Instamart"
         description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
       />
