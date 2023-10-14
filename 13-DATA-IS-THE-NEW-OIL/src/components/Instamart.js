450bed91309572ab75101c7353c2aed0618aa3dd
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-15 03:21:09 +0530

    made some dummy sections in our instamart

--- 13-DATA-IS-THE-NEW-OIL/src/components/Instamart.js
+++ 13-DATA-IS-THE-NEW-OIL/src/components/Instamart.js
@@ -1,6 +1,9 @@
-const Section=({title})=>{
+const Section=({title,description})=>{
   return(
-    <h3>{title}</h3>
+    <div className="border border-black p-2 m-2">
+      <h3 className="font-bold text-xl">{title}</h3>
+      <p>{description}</p>
+    </div>
   )
 }
 
@@ -8,7 +11,12 @@ const Section=({title})=>{
 const Instamart = () => {
   return (
     <div>
-      <Section title="AboutInstaMart" />
+      <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+      <Section title="About Instamart" description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."/>
+
+      <Section title="Team at Instamart" description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."/>
+      
+      <Section title="Careers at Instamart" description="On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."/>
       {/* <AboutInstaMart/>
       <DetailsOfInstamart/>
       <TeamsInstamart/>
