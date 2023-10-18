a95c122c64e6a1c54d0f1907823df57ab53044e8
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-18 18:41:40 +0530

    created our cartSlice and exported our reducer

---
+++ 15-LETS-BUILD-OUR-OWN-STORE/src/utils/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const cartSlice=createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+    },
+    reducers: {
+        addItem: (state,action)=>{
+            state.items.push(action.payload);
+        },
+        removeItem: (state,action)=>{
+            // state.items.pop()
+            const itemToRemove = action.payload;
+            state.items = state.items.filter(item => item !== itemToRemove);
+        },
+        clearCart: (state)=>{
+            state=[];
+        },
+    }
+});
+
+export default cartSlice.reducer;
