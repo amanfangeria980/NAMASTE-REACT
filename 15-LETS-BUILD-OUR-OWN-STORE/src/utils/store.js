e577732ffc95872d153d04ddfa8798316772ff6a
amanfangeria980 <amanfangeria980@gmail.com>
2023-10-18 18:56:06 +0530

    added the steps of what we did in redux and added our cart slice to our store

--- 15-LETS-BUILD-OUR-OWN-STORE/src/utils/store.js
+++ 15-LETS-BUILD-OUR-OWN-STORE/src/utils/store.js
@@ -1,7 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartSlice from "./cartSlice"
 
 const store=configureStore({
-    // slices
+    reducer: {
+        cart: cartSlice,
+    }
 });
 
 export default store;
+
+
+
+
+
+// Steps 
+/* 
+*Create Store:
+    -configureStore() - RTK
+*Provided my store to app
+    -<Provider store={store}> - import from react-redux
+*Created a Slice
+    -RTK - createSlice({
+        name: "",
+        initialState: 0,
+        reducers:{
+            addItem: (state,action)=>{state=action.payload / modify our state}
+        }
+    })
+    export {addItem}=cartSlice.actions;
+    export default cartSlice.reducer;
+*Put that slice into store
+    -{
+        reducer: {
+            cart: cartSlice
+            user: userSlice
+        }
+    }
+
+*/
